6b674896a315484ad015847b14589fe2d8baaf2f
Lucas Barbosa <llbarbosasc@gmail.com>
2021-12-23 01:57:44 -0400

    feat: sign persistence, share button fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import ouroscopoData from "./ouroscopo.json";
 import { useState, useEffect } from "react";
-import { toJpeg } from "html-to-image";
+import { toPng } from "html-to-image";
 
 const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 const getSignByRock = (rock) =>
@@ -117,11 +117,15 @@ function Sign({
   );
 }
 
-function Wisdom({ description, text, ...props }) {
+function Wisdom({ description, text, children, ...props }) {
   return (
-    <div {...props} className="bg-holy-purple-dark rounded-xl pt-3 p-5 mb-5">
+    <div
+      {...props}
+      className="bg-holy-purple-dark rounded-xl pt-3 p-5 mb-5 relative"
+    >
       <span className="text-holy-purple-softer">{description}</span>
       <p className="mt-3 text-lg font-medium">{text}</p>
+      {children}
     </div>
   );
 }
@@ -142,6 +146,18 @@ function ShowSignInfoPage({ signRock, ascendantRock }) {
   const sign = getSignByRock(signRock);
   const ascendant = getSignByRock(ascendantRock);
   const [shareActionError, setShareActionError] = useState(false);
+  const [randomWisdom, setRandomWisdom] = useState("");
+  const [randomQuality, setRandomQuality] = useState("");
+
+  const undoOuroscopo = () => {
+    localStorage.clear();
+    document.location.reload();
+  };
+
+  useEffect(() => {
+    setRandomWisdom(getRandomWisdom(ascendant));
+    setRandomQuality(getRandomQuality(sign));
+  }, [sign, ascendant]);
 
   useEffect(() => {
     const shareButton = document.getElementById("share-button");
@@ -158,22 +174,29 @@ function ShowSignInfoPage({ signRock, ascendantRock }) {
 
   const shareAction = () => {
     const wisdomNode = document.getElementById("wisdom");
+    const shareButton = document.getElementById("share-button");
 
-    toJpeg(wisdomNode)
-      .then(async (dataUrl) => {
-        const blob = await (await fetch(dataUrl)).blob();
-        const filesArray = [
-          new File([blob], "conselho.png", {
-            type: blob.type,
-            lastModified: new Date().getTime(),
-          }),
-        ];
-        const shareData = {
-          files: filesArray,
-        };
-        navigator.share(shareData);
-      })
-      .catch(() => setShareActionError(true));
+    shareButton.style.opacity = "0";
+
+    setTimeout(
+      () =>
+        toPng(wisdomNode)
+          .then(async (dataUrl) => {
+            const blob = await (await fetch(dataUrl)).blob();
+            const filesArray = [
+              new File([blob], "conselho.png", {
+                type: blob.type,
+                lastModified: new Date().getTime(),
+              }),
+            ];
+            navigator.share({
+              files: filesArray,
+            });
+          })
+          .catch(() => setShareActionError(true))
+          .finally(() => (shareButton.style.opacity = "1")),
+      500
+    );
   };
 
   return (
@@ -198,15 +221,19 @@ function ShowSignInfoPage({ signRock, ascendantRock }) {
           "equivalente_descricao"
         )}
       />
-      <Wisdom description="Característica" text={getRandomQuality(sign)} />
-      <Wisdom
-        id="wisdom"
-        description="Conselho para o dia"
-        text={getRandomWisdom(ascendant)}
-      />
-      <div className="pt-5 flex justify-center">
-        <DefaultButton id="share-button" onClick={shareAction}>
-          <ShareIcon />
+      <Wisdom description="Característica" text={randomQuality} />
+      <Wisdom id="wisdom" description="Conselho para o dia" text={randomWisdom}>
+        <DefaultButton
+          id="share-button"
+          onClick={shareAction}
+          className="absolute -right-3 -bottom-5 p-3"
+        >
+          <ShareIcon height="18px" width="18px" />
+        </DefaultButton>
+      </Wisdom>
+      <div className="flex justify-center">
+        <DefaultButton onClick={undoOuroscopo} className="py-4 px-7 font-bold">
+          Refazer Ouroscopo
         </DefaultButton>
       </div>
     </div>
@@ -216,12 +243,12 @@ function ShowSignInfoPage({ signRock, ascendantRock }) {
 function ShareIcon(props) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
       height="24px"
       viewBox="0 0 24 24"
       width="24px"
       fill="#302664"
+      {...props}
     >
       <path d="M0 0h24v24H0V0z" fill="none" />
       <path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92s2.92-1.31 2.92-2.92c0-1.61-1.31-2.92-2.92-2.92zM18 4c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zM6 13c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm12 7.02c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1z" />
@@ -233,7 +260,23 @@ function App() {
   const [signRock, setSignRock] = useState("");
   const [ascendantRock, setAscendantRock] = useState("");
   const [currentPage, setCurrentPage] = useState("selectRocks");
-  const onSubmitRocks = () => setCurrentPage("showSignInfo");
+
+  useEffect(() => {
+    const signFromStorage = localStorage.getItem("sign");
+    const ascendantFromStorage = localStorage.getItem("ascendant");
+
+    if (signFromStorage !== null && ascendantFromStorage !== null) {
+      setSignRock(getSignInfo(signFromStorage, "pedra"));
+      setAscendantRock(getSignInfo(ascendantFromStorage, "pedra"));
+      setCurrentPage("showSignInfo");
+    }
+  }, []);
+
+  const onSubmitRocks = () => {
+    localStorage.setItem("sign", getSignByRock(signRock));
+    localStorage.setItem("ascendant", getSignByRock(ascendantRock));
+    setCurrentPage("showSignInfo");
+  };
 
   const pages = {
     selectRocks: (
